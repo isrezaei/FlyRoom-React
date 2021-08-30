01cd3115dcdce4ea6f32ce898a5c693c5c8cd402
Isrezaei <rezaeiism@yahoo.com>
2021-08-30 13:43:43 +0430

    fly chat v beta 0.9 fix 50% UI Project

--- src/Components/Item-Area/Item-Area.jsx
+++ src/Components/Item-Area/Item-Area.jsx
@@ -8,33 +8,31 @@ export default function ItemArea()
             <div className='ParentIcon'>
 
                 <div className='Logo'>
-                    <img src="https://img.icons8.com/office/80/000000/parrot.png"/>
+                    <img src="https://img.icons8.com/fluency/96/000000/butterfly.png"/>
                 </div>
 
                 <div className='Item'>
                     <div className='Write'>
-                        <img src="https://img.icons8.com/wired/64/000000/ball-point-pen.png"/>
+                        <img src="https://img.icons8.com/ios/100/000000/hand-with-pen.png"/>
                     </div>
                     <div className='People'>
-                        <img src="https://img.icons8.com/wired/64/000000/user-group-man-man.png"/>
+                        <img src="https://img.icons8.com/ios/50/000000/user-group-man-man.png"/>
                     </div>
                     <div className='Message'>
-                        <img src="https://img.icons8.com/wired/64/000000/speech-bubble-with-dots.png"/>
+                        <img src="https://img.icons8.com/ios/100/000000/message-group.png"/>
                     </div>
                     <div className='Alert'>
-                        <img src="https://img.icons8.com/wired/64/000000/appointment-reminders.png"/>
+                        <img src="https://img.icons8.com/ios/50/000000/alarm.png"/>
                     </div>
-                    <div className='Window'>
-                        <img src="https://img.icons8.com/wired/64/000000/minimize-window.png"/>
-                    </div>
+
                     <div className='Setting'>
-                        <img src="https://img.icons8.com/wired/64/000000/settings.png"/>
+                        <img src="https://img.icons8.com/ios/100/000000/settings.png"/>
                     </div>
 
-                    <div className='MyProfile'>
-                        <img/>
-                    </div>
+                </div>
 
+                <div className='MyProfile'>
+                    <img/>
                 </div>
 
 
