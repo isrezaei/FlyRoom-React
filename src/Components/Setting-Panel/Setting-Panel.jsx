6162e08e66d7e101a9d76b9acd165e5255f4aafc
Isrezaei <rezaeiism@yahoo.com>
2021-09-10 13:22:53 +0430

    fly chat v beta 2 fixed building responsive 0 - 480 px Setting-Pane

--- src/Components/Setting-Panel/Setting-Panel.jsx
+++ src/Components/Setting-Panel/Setting-Panel.jsx
@@ -1,4 +1,6 @@
 import React , {useEffect,useRef} from "react";
+import { WiMoonAltWaningCrescent1 } from 'react-icons/wi';
+import { IoReturnUpBackOutline } from 'react-icons/io5';
 
 export default function SettingPanel ({SettingPanelData , SetDarkMood})
 {
@@ -21,51 +23,17 @@ export default function SettingPanel ({SettingPanelData , SetDarkMood})
     return (
         <div ref={SettingPanel} className='Setting-Panel'>
 
-            <p className='Appearance'>Appearance</p>
+
 
             <div className='Dark-Mood'>
                 <p>Dark Mood</p>
-                <button onClick={SetDarkMood}>Set</button>
+                <button onClick={SetDarkMood}><WiMoonAltWaningCrescent1 className='Icone'/></button>
+                <button onClick={ClosePanelSetting}><IoReturnUpBackOutline className='Icone'/></button>
             </div>
 
 
-            <div className='Color-Theme'>
-
-                <div className='Themes'>
-                    <p>Theme 1</p>
-                    <p>Theme 2</p>
-                    <p>Theme 3</p>
-                    <p>Theme 4</p>
-                </div>
-
-                <div className='Handel-Palette'>
-                    <div className='Palette'>
-                        <div className='Cercel N1'></div>
-                        <div className='Cercel N2'></div>
-                    </div>
 
 
-                    <div className='Palette'>
-                        <div className='Cercel N3'></div>
-                        <div className='Cercel N4'></div>
-                    </div>
-
-
-                    <div className='Palette'>
-                        <div className='Cercel N5'></div>
-                        <div className='Cercel N6'></div>
-                    </div>
-
-
-
-                    <div className='Palette'>
-                        <div className='Cercel N7'></div>
-                        <div className='Cercel N8'></div>
-                    </div>
-                </div>
-            </div>
-
-            <button onClick={ClosePanelSetting}>Back</button>
 
         </div>
     )
