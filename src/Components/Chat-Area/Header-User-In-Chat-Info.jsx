c816135d27b9044bd674a72f78396b5e11f1658e
Isrezaei <rezaeiism@yahoo.com>
2021-09-15 18:08:46 +0430

    fly chat v 5 Completed

--- src/Components/Chat-Area/Header-User-In-Chat-Info.jsx
+++ src/Components/Chat-Area/Header-User-In-Chat-Info.jsx
@@ -5,8 +5,8 @@ export default function HeaderUserInChatInfo({RefGoHome , OpenPanel , CloseChatA
     return (
         <div className= 'Header-Sender-and-Reciver-Avatar-Info'>
             <div className='Sender-Recive-Avatar-Info'>
-                <div ref={RefGoHome} onClick={CloseChatArea} className='BackToHome'>
-                    <img src="https://img.icons8.com/fluency/96/000000/home.png"/>
+                <div ref={RefGoHome} onClick={CloseChatArea}>
+                    <img className='BackToHome' src="https://img.icons8.com/fluency/96/000000/home.png"/>
                 </div>
                 <div onClick={OpenPanel} className='Click-To-Info'>
                     <img src="https://img.icons8.com/color/96/000000/information--v2.png"/>
